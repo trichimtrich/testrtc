d0b9e377afcbb5e2c4ea6ad7c6f33298757f1603
hasagi <trichimtrich@gmail.com>
2019-06-20 18:05:44 +0800

    add bin for js cli

---
+++ bin/jsclient_docker.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ -e main.go ]; then
+    WORKDIR="./"
+elif [ -e ../main.go ]; then
+    WORKDIR="../"
+else
+    echo "[!] Please change directory to bin or root of the repo"
+    exit 1
+fi
+
+if [ $(id -u) != 0 ]; then
+    echo "[!] Please run as root"
+    exit 1
+fi
+
+docker build --no-cache -t testrtc-img $WORKDIR
+docker run -it testrtc-img node chrome.js $@
